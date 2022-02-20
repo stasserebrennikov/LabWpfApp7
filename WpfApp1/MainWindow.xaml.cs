253d5c314ee59b0f6464da88b69a76d25d18d617
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2022-02-21 00:33:41 +0300

    Добавление стилей

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -132,5 +132,13 @@ namespace WpfApp1
             Application.Current.Shutdown();
 
         }
+
+        private void themes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Uri theme = new Uri(themes.SelectedIndex == 0? "Light.xaml" : "Dark.xaml",UriKind.Relative);
+            ResourceDictionary themeDict = Application.LoadComponent(theme) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Add(themeDict);
+        }
     }
 }
